dcb98c6e6d52ce06e3cd7dbc532d3f7c46d42424
eundo <eundo93@gmail.com>
2023-09-05 14:24:03 +0900

    빌드오류 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -123,8 +123,15 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/eundo',
               },
+              {
+							html: `
+                <a href="https://www.netlify.com" target="_blank" rel="noreferrer noopener" aria-label="Deploys by Netlify">
+                  <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" />
+                </a>
+              `,
+						  },
             ],
           },
         ],
