b166dad7769753647a1a4f205a166ffe34fa740d
Eundo Park <eundo@cnspartner.com>
2024-09-10 11:27:16 +0900

    Google Analytics 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,24 @@ const config = {
       }),
     ],
   ],
-
+  headTags: [
+    {
+      tagName: 'script',
+      attributes: {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-VG06L597YE',
+      },
+    },
+    {
+      tagName: 'script',
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-VG06L597YE');
+      `,
+    },
+  ],
   themeConfig:
       ({
         image: '/static/img/ped1.jpg',
@@ -97,10 +114,6 @@ const config = {
           //     position: 'right',
           //   },
           // ],
-        },
-        googleAnalytics: {
-          trackingID: 'G-VG06L597YE',
-          anonymizeIP: false, // 선택 사항: IP 익명화 설정
         },
         footer: {
           style: 'dark',
